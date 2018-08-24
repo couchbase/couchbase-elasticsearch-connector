d5ba38d779488de7a15d48bcfa0c1223cbbf77ec
David Nault <david.nault@couchbase.com>
2018-08-23 17:32:16 -0700

    Clearing the slate for version 4 development

--- src/main/java/org/elasticsearch/transport/couchbase/capi/TypeSelector.java
+++
@@ -1,9 +0,0 @@
-package org.elasticsearch.transport.couchbase.capi;
-
-import org.elasticsearch.common.settings.Settings;
-
-public interface TypeSelector {
-    void configure(Settings settings);
-
-    String getType(String index, String docId);
-}
