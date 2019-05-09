7c50ba14ab309d9725783bd0377b9749225b7ee3
David Nault <david.nault@couchbase.com>
2019-05-09 14:27:37 -0700

    CBES-126 Fix rare race condition that only affects integration tests.
    Motivation
    ==========
    The tests run multiple connector instances in the same process,
    and the instances all compete to configure gauges.
    
    If two connectors try to configure the `cbes.esWaitMs` gauge at the
    same time, an exception is thrown with the message:
    "cbes.esWaitMs is already used for a different type of metric"
    
    Modifications
    =============
    Synchronize so only one connector instance can configure the gauges
    at a time.
    
    Change-Id: I38fdbcd417ffea61fa4383b169ab6f7f6161adae
    Reviewed-on: http://review.couchbase.org/108907
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: David Nault <david.nault@couchbase.com>

--- src/main/java/com/couchbase/connector/elasticsearch/Metrics.java
+++ src/main/java/com/couchbase/connector/elasticsearch/Metrics.java
@@ -79,8 +79,13 @@ public class Metrics {
   public static Gauge gauge(String name, MetricRegistry.MetricSupplier<Gauge> supplier) {
     // Some of our gauges are backed by connections to Couchbase or other server.
     // These must be recreated for each connection, so remove first.
-    registry.remove(PREFIX + name);
-    return registry.gauge(PREFIX + name, supplier);
+    synchronized (Metrics.class) {
+      // The metrics registry is a singleton, but the integration tests run many connectors in the
+      // same process and they all compete to register gauges. Synchronize to prevent rare race condition
+      // that can cause exception to be thrown here when running integration tests.
+      registry.remove(PREFIX + name);
+      return registry.gauge(PREFIX + name, supplier);
+    }
   }
 
   public static MetricRegistry registry() {
